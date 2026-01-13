7d0672da47deb086ff6c0e0c95f7ccd121e65ea1
yasmine gharsallah <yasminegharssalah2023@gmail.com>
2026-01-14 00:10:52 +0100

    correction

--- src/test/java/art/org/example/gestion_des_conges/GestionDesCongesApplicationTests.java
+++ src/test/java/art/org/example/gestion_des_conges/GestionDesCongesApplicationTests.java
@@ -2,8 +2,10 @@ package art.org.example.gestion_des_conges;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestionDesCongesApplicationTests {
 
     @Test
